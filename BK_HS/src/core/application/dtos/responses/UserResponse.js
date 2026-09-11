e37f95f1fe459a784b0287f3a74f80e41960281b
José David Amaya Mosquera <josedam2704@gmail.com>
2026-09-11 10:30:45 -0500

    Arreglado

--- BK_HS/src/core/application/dtos/responses/UserResponse.js
+++ BK_HS/src/core/application/dtos/responses/UserResponse.js
@@ -1,4 +1,21 @@
 class UserResponse {
-  static fromEntity(user) { return { userId: user.id, username: user.username, email: user.email, status: user.status, fullName: user.fullName, documentType: user.documentType, documentNumber: user.documentNumber, phone: user.phone, city: user.city, avatarDataUrl: user.avatarDataUrl, createdAt: user.createdAt }; }
+  static fromEntity(user) {
+    return {
+      userId: user.id,
+      username: user.username,
+      email: user.email,
+      status: user.status,
+      fullName: user.fullName,
+      documentType: user.documentType,
+      documentNumber: user.documentNumber,
+      phone: user.phone,
+      city: user.city,
+      avatarDataUrl: user.avatarDataUrl,
+      createdAt: user.createdAt,
+    };
+  }
 }
-module.exports = { UserResponse };
+
+module.exports = {
+  UserResponse,
+};
